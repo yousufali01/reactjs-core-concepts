0eaebc237141eb1b08c51558bd5ca1ebf1e664a7
yousufali01 <yousufali.ya01@gmail.com>
2022-09-28 22:42:19 +0600

    reactjs-core-concepts

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    {name: 'laptop', price: 20000},
+    {name: 'phone', price: 10000},
+    {name: 'watch', price: 50000},
+    {name: 'tablet', price: 750000}
+  ]
   return (
     <div className="App">
-      <header className="App-header">
+
+
+
+      
+      {
+        products.map(product=> <Product name={product.name} price={product.price}></Product>)
+      }
+      
+      {/* <Product name="laptop" price="1,000000 Tk"></Product>
+      <Product name="Phone" price="75,000 Tk"></Product>
+      <Product name="watch" price="5,000 Tk"></Product> */}
+
+
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +37,18 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
+function Product(props){
+  return(
+    <div className="product">
+      <h3>Name: {props.name}</h3>
+      <p>Price: {props.price}</p>
+    </div>
+  )
+}
+
 
 export default App;
